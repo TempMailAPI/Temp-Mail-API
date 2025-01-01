3978d49fe86a344ecb84431261004aeddfa731f3
rexshi <shirui@gmail.com>
2025-01-01 19:28:41 +0800

    Update setup.py

--- Python/setup.py
+++ Python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="temp-mail-so",
-    version="1.0.0",
+    version="1.0.1",
     author="TempMail.so",
     author_email="service@tempmail.so",
     description="A Python SDK for TempMail.so temporary email service",
